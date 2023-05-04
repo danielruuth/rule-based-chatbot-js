a0a796c67af22f1fe0f88a2005f6433a26c531bd
danielruuth <daniel.ruuth@gmail.com>
2023-05-04 10:01:43 +0200

    Added support for scripted conversations i.e. questions

--- simple-nlp.js
+++ simple-nlp.js
@@ -38,6 +38,18 @@ class Simple_Nlp {
         this.__Rules = Object.create(null);
         this.__Answers = Object.create(null);
         this.__Events = Object.create(null);
+        this.__Manuscript = []
+        this.__onScriptedThread = false;
+        this.__scriptDoneCallback = null
+    }
+
+    set manuscript(questions){
+        this.__Manuscript = questions
+        this.__onScriptedThread = true
+    }
+
+    set manuscriptCallback(callback){
+        this.__scriptDoneCallback = callback
     }
 
     set rules(rules){
@@ -74,26 +86,37 @@ class Simple_Nlp {
     }
 
     process(str) {
-        const result = Object.create(null);
-        result.utterance = str;
-        result.possibleAnswers = Array();
-        result.classifications = Object.create(null);
-        for(let marker in this.__Rules) {
-            // let match = str.match(this.__Rules[marker]);
-            let match = this.__Rules[marker].exec(str);
-            if(match) {
-                result.classifications[marker] = match;
-                result.possibleAnswers.push(processData(match, this.__Answers[marker]))
-                //Cast an event
-                if(this.__Events[marker]){
-                    console.log('Dispatching event')
-                    dispatchEvent(`on${this.__Events[marker]}`)
+        if(this.__onScriptedThread && this.__Manuscript.length > 0){
+            const result = this.__Manuscript.shift()
+
+            if(this.__Manuscript.length == 0){
+                this.__onScriptedThread = false;
+                if(this.__scriptDoneCallback){
+                    this.__scriptDoneCallback()
                 }
             }
+        }else{
+            const result = Object.create(null);
+            result.utterance = str;
+            result.possibleAnswers = Array();
+            result.classifications = Object.create(null);
+            for(let marker in this.__Rules) {
+                // let match = str.match(this.__Rules[marker]);
+                let match = this.__Rules[marker].exec(str);
+                if(match) {
+                    result.classifications[marker] = match;
+                    result.possibleAnswers.push(processData(match, this.__Answers[marker]))
+                    //Cast an event
+                    if(this.__Events[marker]){
+                        console.log('Dispatching event')
+                        dispatchEvent(`on${this.__Events[marker]}`)
+                    }
+                }
+            }
+            result.possibleAnswers = flattenArray(result.possibleAnswers);
+            result.answer = result.possibleAnswers[randomBetween(0, result.possibleAnswers.length - 1)];
+            return result;
         }
-        result.possibleAnswers = flattenArray(result.possibleAnswers);
-        result.answer = result.possibleAnswers[randomBetween(0, result.possibleAnswers.length - 1)];
-        return result;
     }
 }
 
